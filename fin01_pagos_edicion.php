a2ae8ccb189267588022152a37c31a99ec773eb6
Alejandro CG <ceron.guzman.alejandro@gmail.com>
2020-02-27 17:38:34 -0600

    ADD Ignore

--- fin01_pagos_edicion.php
+++ fin01_pagos_edicion.php
@@ -392,7 +392,7 @@
 			<footer role="contentinfo">
 				<div class="clearfix">
 					<ul class="list-unstyled list-inline pull-left">
-						<li>Top MKT &copy; 2018</li>
+						<li>AlejandroCG&copy; 2018</li>
 					</ul>
 					<button class="pull-right btn btn-inverse-alt btn-xs hidden-print" id="back-to-top"><i class="fa fa-arrow-up"></i></button>
 				</div>
